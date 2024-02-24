2df5b17ad85b34ee2b78f663dfd0a02bc0cddb5c
Mariia Kryvokhata <m.krivokhata@gmail.com>
2024-02-24 15:19:05 +0200

    Add the logic of main ( )

--- main.go
+++ main.go
@@ -8,5 +8,11 @@ import (
 )
 
 func main() {
+	handler := http.HandlerFunc(getTime)
+	http.Handle("/time", handler)
+	http.ListenAndServe(":8795", nil)
+}
+
+func getTime(w http.ResponseWriter, r *http.Request) {
 
 }
